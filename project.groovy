d02c152d3c90320d0e31566e49b209b6e60e9bac
Prathamesh <p@gmail.com>
2024-04-07 15:42:38 +0530

    new

--- project.groovy
+++ project.groovy
@@ -3,41 +3,8 @@ pipeline {
     stages {
         stage('Build') { 
             steps {
-                    
-                echo "Cloning the repos"
+            echo 'Cloning the repos'
             }
         }
-
-        stage('Develop')
-        {
-            steps{
-                echo "Developing the code "
-            }
-        }
-
-        stage('Test')
-        {
-            steps{
-                echo "Testing the code"
-
-            }
-        }
-        
-        stage('Quality test')
-        {-
-            steps{
-                echo "Testing the quality of the project"
-
-            }
- 
-        }
-
-        stage('Deploy')
-        {
-            steps{
-                echo "Successfully Deploy"
-            }
-        }
-
-    }
-}
+    } 
+}   
