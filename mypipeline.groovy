f77d47b2065adc01d809747cfac517425ec8e2c2
Prathamesh <p@gmail.com>
2024-04-07 14:38:10 +0530

    New msg

--- mypipeline.groovy
+++ mypipeline.groovy
@@ -7,6 +7,38 @@ pipeline {
                 echo "Cloning the repos"
             }
         }
+
+        stage('Develop')
+        {
+            steps{
+                echo "Developing the code "
+            }
+        }
+
+        stage('Test')
+        {
+            steps{
+                echo "Testing the code"
+
+            }
+        }
+        
+        stage('Quality test')
+        {
+            steps{
+                echo "Testing the quality of the project"
+
+            }
+ 
+        }
+
+        stage('Deploy')
+        {
+            steps{
+                echo "Successfully Deploy"
+            }
+        }
+
     }
 }
 
